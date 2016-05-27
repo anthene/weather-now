aa3866cb417492114fe2a297020003a1110f7a10
Anton Medvedev <anton.medvedev@mandosys.com>
2016-05-27 13:30:29 +0300

    #1: Simple one page site with 3 icons, script and style

---
+++ script.js
@@ -0,0 +1,75 @@
+var dict = {
+	Thunderstorm: "Гроза",
+	Drizzle: "Мелкий дождь",
+	Rain: "Дождь",
+	Snow: "Снег",
+	Atmosphere: "Atmosphere",
+	Clear: "Ясно",
+	Clouds: "Облачно",
+	Extreme: "Extreme",
+	Additional: "Additional",
+}
+
+function setInnerHtml(elemId, value) {
+	var elem = document.getElementById(elemId);
+	
+	if (elem)
+		elem.innerHTML = value;
+}
+
+function fillForm(data, prefix) {
+	if (prefix === undefined)
+		prefix = "";
+		
+	setInnerHtml(prefix + "dt", new Date(1000*data.dt).toLocaleString("ru"));
+	// setInnerHtml(prefix + "main", data.weather[0].main in dict ? dict[data.weather[0].main] : data.weather[0].main);
+	setInnerHtml(prefix + "pic", "<img src=\"" + data.weather[0].main + ".png\"></img>");
+	if (data.main) {
+		setInnerHtml(prefix + "main.temp", Math.round(data.main.temp) + "°C");
+		setInnerHtml(prefix + "pressure", Math.round(data.main.pressure*0.7500638) + " мм рт. ст.");
+		setInnerHtml(prefix + "humidity", data.main.humidity) + "%";
+	}
+	else {
+		setInnerHtml(prefix + "main.temp-range", Math.round(data.temp.min) + "°C.." + Math.round(data.temp.max) + "°C");
+		setInnerHtml(prefix + "pressure", Math.round(data.pressure*0.7500638) + " мм рт. ст.");
+	}
+	// setInnerHtml(prefix + "pressure", Math.round(data.main.pressure*0.7500638) + " мм рт. ст.");
+	// setInnerHtml(prefix + "wind.speed", data.wind.speed);
+	// setInnerHtml(prefix + "sunrise", new Date(1000*data.sys.sunrise));
+	// setInnerHtml(prefix + "sunset", new Date(1000*data.sys.sunset));
+}
+
+function getJson(callback, request) {
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('GET', request, true);
+	
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState != 4) return;
+		
+		if (xhr.status === 200) {
+			callback(JSON.parse(xhr.responseText));
+		}
+	}
+
+	xhr.send();
+}
+
+function getWeatherData(callback) {
+	getJson(callback, 'http://api.openweathermap.org/data/2.5/weather?id=524901&units=metric&appid=0da70d33c9a5dfaf3ded356599ea6929');
+}
+
+function getForecastData(callback) {
+	getJson(callback, 'http://api.openweathermap.org/data/2.5/forecast/daily?id=524901&cnt=2&units=metric&appid=0da70d33c9a5dfaf3ded356599ea6929');
+}
+
+function startGetWeatherData() {
+	setInterval(function () { getWeatherData(fillForm); }, 60000);
+}
+
+getWeatherData(fillForm);
+getForecastData(function (data) {
+	fillForm(data.list[0], "tod-");
+	fillForm(data.list[1], "tom-");
+});
+//<!-- startGetWeatherData(); -->
